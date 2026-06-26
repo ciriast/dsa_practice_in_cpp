60e2b79df972a972442859d134708ba6065ec26d
ciriast <ciriastorres@gmail.com>
2026-06-26 06:08:13 -0600

    Binary search working in cpp, it feels good!

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
+#include <vector>
 
-int binary_search()
+int binary_search(std::vector<int> nums, int key)
 {
-    return 100;
+    int start = 0;
+    int end = nums.size() - 1;
+
+    while (start <= end) 
+    {
+        int mid = (start + end) / 2;
+
+        if (nums[mid] == key)
+        {
+            return mid;
+        }
+
+        if (nums[mid] < key)
+        {
+            start = mid + 1;
+        }
+
+        if (nums[mid] > key)
+        {
+            end = mid - 1;
+        }
+    
+    }
+
+    return -1;
 }
 
 int main()
 {
-    int my_value = binary_search();
-    std::cout << my_value;
+    std::vector<int> my_nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int my_key = binary_search(my_nums, 7);
+    std::cout << my_key;
 }
