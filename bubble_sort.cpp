8ec630920b9ca1271089f295dd8fa917d100e208
ciriast <ciriastorres@gmail.com>
2026-06-26 07:23:11 -0600

    Bubble sort completedgit add .!

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,11 +3,37 @@
 
 std::vector<int> bubble_sort(std::vector<int> nums) 
 {
+    bool swapping = true;
+    int end = nums.size() - 1;
+
+    while (swapping) 
+    {
+        swapping = false;
+
+        for (int i = 1; i <= end; ++i) 
+        {
+            if (nums[i - 1] > nums[i]) 
+            {
+                int temp = nums[i - 1];
+                nums[i - 1] = nums[i];
+                nums[i] = temp;
+                swapping = true;
+            }
+        }
+        
+        end -= 1;
+    }
+
     return nums;
 }
 
 int main() 
 {
     std::vector<int> my_nums = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    std::cout << bubble_sort(my_nums)
+    std::vector<int> my_sorted_nums = bubble_sort(my_nums);
+    
+    for (int i: my_sorted_nums)
+    {
+        std::cout << i << " ";
+    }
 }
