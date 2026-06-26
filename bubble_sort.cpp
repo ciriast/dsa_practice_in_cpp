8a988ce31eef1bd5b8390d3c51244905008798ef
ciriast <ciriastorres@gmail.com>
2026-06-26 06:21:31 -0600

    Starting bubble sort

---
+++ bubble_sort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> bubble_sort(std::vector<int> nums) 
+{
+    return nums;
+}
+
+int main() 
+{
+    std::vector<int> my_nums = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    std::cout << bubble_sort(my_nums)
+}
